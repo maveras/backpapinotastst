5bfddeb4405a8f9e9cc47885514af974cdc8fc62
Marcelo Vera <marcelo.verasalvatierra@gmail.com>
2019-09-12 21:21:33 -0300

    fix correct place to make querys

--- app/graphql/types/mutation_type.rb
+++ app/graphql/types/mutation_type.rb
@@ -7,20 +7,4 @@ module Types
       "Hello World"
     end
   end
-  class QueryType < Types::BaseObject
-
-    field :users, [Types::UserType], null: false
-
-    def users
-      User.all
-    end
-
-    field :user, Types::UserType, null: false do
-      argument :id, ID, required: true
-    end
-
-    def user(id:)
-      User.find(id)
-    end
-  end
 end
