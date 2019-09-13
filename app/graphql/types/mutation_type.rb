41ba17e98d859269c6ca4d92c7bb46d6742da1fa
Marcelo Vera <marcelo.verasalvatierra@gmail.com>
2019-09-12 21:06:11 -0300

    added user tyoe to graph and mutation types

--- app/graphql/types/mutation_type.rb
+++ app/graphql/types/mutation_type.rb
@@ -7,4 +7,19 @@ module Types
       "Hello World"
     end
   end
+  class QueryType < Types::BaseObject
+
+    field :users, [Types::UserType], null: false
+
+    def users
+      User.all
+    end
+
+    # field :user, Types::UserType, null: false do
+    #   argument :id, ID, required: true
+    # end
+
+    # def user(id:)
+    #   User.find(id)
+    end
 end
--- app/graphql/types/user_type.rb
+++ app/graphql/types/user_type.rb
@@ -1,4 +1,10 @@
 module Types
   class UserType < Types::BaseObject
+    field :id, ID, null: false
+    field :rut, String, null: true
+    field :name, String, null: true
+    field :last_name, String, null: true
+    field :prescense, String, null: true
+    field :n_list, String, null: false
   end
 end
